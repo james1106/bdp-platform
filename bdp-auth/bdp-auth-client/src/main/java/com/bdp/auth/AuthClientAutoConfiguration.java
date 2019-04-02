c5d6d3efbdbf3f2c958995311ba18810a6934e1d
jinkunaier <465110675@qq.com>
2019-04-02 22:58:17 +0800

    更新一些描述不准确的地方

--- bdp-auth/bdp-auth-client/src/main/java/com/bdp/auth/AuthClientAutoConfiguration.java
+++ bdp-auth/bdp-auth-client/src/main/java/com/bdp/auth/AuthClientAutoConfiguration.java
@@ -11,14 +11,14 @@ import com.bdp.auth.hystrix.HystrixConcurrencyStrategyConfig;
 @Configuration
 // @ComponentScan
 // @EnableResourceServer
-// 注意：上面行的配置与下面一行配置是等价的，这里使用下面这种方式是为了@EnableBdpAuthClient共用AuthClientAutoConfig，代码一致些
+// 注意：上面两行的配置与下面这一行配置是等价的，这里使用下面这种方式是为了@EnableBdpAuthClient共用AuthClientAutoConfig，代码一致些
 @Import({ AuthClientAutoConfig.class, HystrixConcurrencyStrategyConfig.class })
 // 很重要，注意看@ConditionalOnBean的解释，因为当前bdp-auth-client被其它项目引用时也通过spring.factories自动配置,<br/>
 // 与OAuth2AutoConfiguration配置是一样方式，而OAuth2AutoConfiguration引入的OAuth2ResourceServerConfiguration依赖ResourceServerConfiguration的存在。<br/>
 // 而ResourceServerConfiguration又是通过@EnableResourceServer引入的，所以当前的自动配置应该在OAuth2AutoConfiguration的自动配置之前执行，<br/>
 // 否则OAuth2ResourceServerConfiguration不生效会导致@EnableResourceServer最终配置错误。<br/>
 // 原则上通过Auto-configuration自动配置依赖的bean不要由其它Auto-configuration包来提供。因为两个自动配置的执行顺序不能确定。而应该由用户自定义bean来提供，<br/>
-// 注意此处Auto-configuration配置特指通过spring.factories加载的配置，这种是"真的"自动配置，通过@EnableResourceServer类似的配置不是人为干预了，不是"真的"自动配置。<br/>
+// 注意此处Auto-configuration配置特指通过spring.factories加载的配置，这种是"真的"自动配置，通过@EnableResourceServer类似的配置是人为干预了，不是"真的"自动配置。<br/>
 // 所谓用户自定义的bean,即通过当前springboot应用扫描配置能够扫描到的bean,而不是通过spring.factories这种方式配置的bean.<br/>
 // @ConditionalOnBean只会查找当前执行时已经放到applicationContext中的bean,此时其它的Auto-configuration可能还没有执行。<br/>
 // 另外@AutoConfigureAfter这样的注解也只是应用在Auto-configuration配置类之间的<br/>
--- bdp-auth/bdp-auth-client/src/main/java/com/bdp/auth/EnableBdpAuthClient.java
+++ bdp-auth/bdp-auth-client/src/main/java/com/bdp/auth/EnableBdpAuthClient.java
@@ -9,14 +9,23 @@ import org.springframework.context.annotation.Import;
 import com.bdp.auth.config.AuthClientAutoConfig;
 import com.bdp.auth.hystrix.HystrixConcurrencyStrategyConfig;
 
+/*
+ * @EnableBdpAuthClient与AuthClientAutoConfiguration 是两种不同的自动装配方式
+ * 前一种是有人为预算的自动装配，即在代码中显示指定@EnableBdpAuthClient，而后面一种是“真的”
+ * 自动装配，其依赖的是spring.factories中文件的配置。即如果当前公用jar包在类路径下，spring框架会自动
+ * 解析META-INF中的spring.factories文件，以实现自动装配。两者可任选其一使用。
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Import({ AuthClientAutoConfig.class, HystrixConcurrencyStrategyConfig.class })
-// 注意：不能直接用下面这两个代替@Import(AuthClientAutoConfig.class)，从而省掉AuthClientAutoConfig类，
-// 因为@ComponentScan添加到类上与添加到其他注解上作用不同，添加到其它注解上面好像不起作用，原因暂时还没搞明白。<br/>
-// 基本所有SpringBoot中的@EnableXxx的注解定义格式与当前注解一样。在做共用的JAR时，尽量不要使用@ComponentScan的注解来做扫描，
-// 用@Import及各种Conditional来完成依赖的注入，参考SpringBoot的其它@EnableXxx的实现
+/*
+ * 注意：不能直接用下面这两个代替@Import(AuthClientAutoConfig.class)，从而省掉AuthClientAutoConfig类，
+ * 因为@ComponentScan添加到类上与添加到其他注解上作用不同，添加到其它注解上面好像不起作用，原因暂时还没搞明白。<br/>
+ * 基本所有SpringBoot中的@EnableXxx的注解定义格式与当前注解一样。在做公用的JAR时，
+ * 尽量不要使用 @ComponentScan的注解来做扫描，
+ * 要用@Import及各种Conditional来完成依赖的注入，参考SpringBoot的其它@EnableXxx的实现
+ */
 // @ComponentScan
 // @EnableResourceServer
 public @interface EnableBdpAuthClient {
